ae7901649fc6159cf16ee403a231aade257cb3b7
Suhaib Mujahid <suhaibmujahid@gmail.com>
2021-06-08 15:49:38 -0400

    chore: Update the team member list

--- src/pages/team.js
+++ src/pages/team.js
@@ -13,9 +13,18 @@ const TeamMemberList = [
     members: [
       {
         name: "Dr. Emad Shihab",
+        role: "Director",
         affiliation: "Concordia University",
+        linkedin: "emad-shihab-8099523",
         twitter: "EmadShihab",
         website: "http://das.encs.concordia.ca/members/emad-shihab/",
+      },
+      {
+        name: "Dr. Foutse Khomh",
+        affiliation: "Polytechnique Montréal",
+        linkedin: "khomh",
+        twitter: "SWATLab",
+        website: "http://swat.polymtl.ca/~foutsekh/about/index.html",
       },
       {
         name: "Dr. Tanja Tajmel",
@@ -26,108 +35,121 @@ const TeamMemberList = [
       {
         name: "Dr. Jinqiu Yang",
         affiliation: "Concordia University",
-        // twitter: "",
+        twitter: "JinqiuYang",
         website: "https://jinqiuyang.github.io",
-      },
-      {
-        name: "Dr. Foutse Khomh",
-        affiliation: "Polytechnique Montréal",
-        twitter: "SWATLab",
-        website: "http://swat.polymtl.ca/~foutsekh/about/index.html",
       },
       {
         name: "Dr. Ying (Jenny) Zou",
         affiliation: "Queen’s University",
-        // twitter: "",
+        linkedin: "jinqiu-yang-69767220",
+        twitter: "YingZou8",
         website: "https://www.ece.queensu.ca/people/Y-Zou/",
       },
       {
         name: "Dr. Sarah Nadi",
         affiliation: "University of Alberta",
+        linkedin: "sarah-nadi-b312b665",
         twitter: "sarahnadi",
         website: "https://sarahnadi.org/",
       },
       {
         name: "Dr. Ahmed E. Hassan",
         affiliation: "Queen’s University",
-        // twitter: "",
+        linkedin:"ahmed-e-hassan",
+        twitter: "sail_lab",
         website: "https://research.cs.queensu.ca/home/ahmed/home/",
       },
     ],
   },
   {
-    title: "Academic Collaborators",
-    members: [
-      {
-        name: "Dr. Gerd Oberleitner",
-        affiliation: "University of Graz",
-        position: "UNESCO Chair in Human Rights and Human Security",
-        imgURL: defaultProfile,
-      },
-      {
-        name: "Dr. Klaus Starl",
-        affiliation: "University of Graz",
-        position: "UNESCO Chair in Human Rights and Human Security",
-        imgURL: defaultProfile,
-      },
-      {
-        name: "Dr. Renji Butalid",
-        affiliation: "Montreal AI Ethics Institute",
-        position: "Co-founder",
-      },
-      {
-        name: "Dr. Lyse Langlois",
-        affiliation: "OBVIA",
-        position: "Executive Director",
-      },
-    ],
+    title: "Students",
+    placeholder: (
+      <h3
+        style={{
+          textAlign: "center",
+          fontSize: "4rem",
+          padding: "5rem 0",
+          color: "var(--ifm-color-emphasis-400)",
+        }}
+      >
+        Coming Soon ...
+      </h3>
+    ),
+    // members: [],
   },
-  {
-    title: "Industrial Collaborators",
-    members: [
-      {
-        name: "Dr. Gilles Savard",
-        affiliation: "IVADO",
-      },
-      {
-        name: "Dr. Julien Crowe",
-        affiliation: "National Bank of Canada",
-      },
-      {
-        name: "Prasad Garigipati",
-        affiliation: "Ericsson",
-      },
-      {
-        name: "Patrick Whelan",
-        affiliation: "TD Bank Group",
-        imgURL: defaultProfile,
-      },
-      {
-        name: "Katherine Heighington",
-        affiliation: "TD Bank Group",
-      },
-      {
-        name: "Stephane Caron",
-        affiliation: "Radio Canada",
-      },
-      {
-        name: "Mohamed El-Geish",
-        affiliation: "Cisco",
-      },
-      {
-        name: "Derek Truong",
-        affiliation: "IBM",
-      },
-      {
-        name: "Pape Wade",
-        affiliation: "Airudi",
-      },
-      {
-        name: "David Fab",
-        affiliation: "GIRO Inc.",
-      },
-    ],
-  },
+  // {
+  //   title: "Academic Collaborators",
+  //   members: [
+  //     {
+  //       name: "Dr. Gerd Oberleitner",
+  //       affiliation: "University of Graz",
+  //       position: "UNESCO Chair in Human Rights and Human Security",
+  //       imgURL: defaultProfile,
+  //     },
+  //     {
+  //       name: "Dr. Klaus Starl",
+  //       affiliation: "University of Graz",
+  //       position: "UNESCO Chair in Human Rights and Human Security",
+  //       imgURL: defaultProfile,
+  //     },
+  //     {
+  //       name: "Dr. Renji Butalid",
+  //       affiliation: "Montreal AI Ethics Institute",
+  //       position: "Co-founder",
+  //     },
+  //     {
+  //       name: "Dr. Lyse Langlois",
+  //       affiliation: "OBVIA",
+  //       position: "Executive Director",
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: "Industrial Collaborators",
+  //   members: [
+  //     {
+  //       name: "Dr. Gilles Savard",
+  //       affiliation: "IVADO",
+  //     },
+  //     {
+  //       name: "Dr. Julien Crowe",
+  //       affiliation: "National Bank of Canada",
+  //     },
+  //     {
+  //       name: "Prasad Garigipati",
+  //       affiliation: "Ericsson",
+  //     },
+  //     {
+  //       name: "Patrick Whelan",
+  //       affiliation: "TD Bank Group",
+  //       imgURL: defaultProfile,
+  //     },
+  //     {
+  //       name: "Katherine Heighington",
+  //       affiliation: "TD Bank Group",
+  //     },
+  //     {
+  //       name: "Stephane Caron",
+  //       affiliation: "Radio Canada",
+  //     },
+  //     {
+  //       name: "Mohamed El-Geish",
+  //       affiliation: "Cisco",
+  //     },
+  //     {
+  //       name: "Derek Truong",
+  //       affiliation: "IBM",
+  //     },
+  //     {
+  //       name: "Pape Wade",
+  //       affiliation: "Airudi",
+  //     },
+  //     {
+  //       name: "David Fab",
+  //       affiliation: "GIRO Inc.",
+  //     },
+  //   ],
+  // },
 ];
 
 export default function Team() {
