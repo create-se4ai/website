dc90d53062962557d5530d2588099a592f086b72
Suhaib Mujahid <suhaibmujahid@gmail.com>
2021-06-08 21:19:22 -0400

    chore: Add a pre-spinner to the apply page

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import Layout from "@theme/Layout";
 import PageHeader from "../components/PageHeader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default () => {
-  useEffect(() => {
+  useEffect(() => { 
     const script = document.createElement("script");
     script.src =
       "https://s3.amazonaws.com/files.freshteam.com/production/78710/attachments/5000279106/original/5000017287_widget.js?1622426024";
@@ -15,7 +17,16 @@ export default () => {
   return (
     <Layout title="Open Positions" description="Apply Now">
       <main>
-        <div id="freshteam-widget"></div>
+        <div id="freshteam-widget">
+          <div
+            style={{
+              padding: "3rem 0 0 1rem",
+              textAlign: "center",
+            }}
+          >
+            <FontAwesomeIcon size={"4x"} spin icon={faSpinner} />
+          </div>
+        </div>
       </main>
     </Layout>
   );
