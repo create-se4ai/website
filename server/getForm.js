329563c9ff96926e7a312a6cee05bb3afaa0e65b
Rawan Abdellatif <rawan.k.abdellatif@gmail.com>
2023-12-31 00:23:41 -0500

    fix bug in getForm in server

--- server/getForm.js
+++ server/getForm.js
@@ -1,5 +1,5 @@
 "use strict";
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 
@@ -10,18 +10,35 @@ const options = {
 };
 
 const getForm = async (req, res) => {
-  const client = new MongoClient(MONGO_URI, options);
+  const studentId = req.params.studentId;
+  // const formId = req.params.formId;
 
   try {
+    const client = new MongoClient(MONGO_URI, options);
     await client.connect();
+
     const db = client.db("se4ai");
-    const forms = await db.collection("FormData").find().toArray();
-    res.status(200).json({ data: forms });
-  } catch (err) {
-    console.error("Error retrieving form data:", err);
-    res.status(500).json({ message: "Error retrieving the Form" });
+
   
-  }
+
+    // Convert formId to ObjectId
+
+    const form = await db.collection("FormData").findOne({ studentId:studentId});
+
+    console.log("Form", form);
+
+    if (!form) {
+      return res.status(404).json({ message: "Form not found" });
+    }
+
+    res.status(200).json({ message: "This is the Form", data: form });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error retrieving form" });
+  } 
+  // finally {
+  //   await client.close(); // Use await when closing the connection
+  // }
 };
 
 module.exports = { getForm };
