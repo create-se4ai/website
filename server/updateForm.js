01508848c2897d9936fd992eaa736254c541483c
Rawan Abdellatif <rawan.k.abdellatif@gmail.com>
2024-01-08 16:06:14 -0500

    add updateForm

--- server/updateForm.js
+++ server/updateForm.js
@@ -15,18 +15,19 @@ const updateForm = async (req, res) => {
     await client.connect();
     const db = client.db("se4ai");
 
-    const { id } = req.params; // Assuming the ID is in the request parameters
+    // const { formId } = req.params;
+    const { studentId } = req.params;
 
     // Check if the provided ID is a valid UUID
-    if (!uuidv4(id)) {
-      return res.status(400).json({ message: "Invalid form ID" });
-    }
+    // if (!uuidv4(formId)) {
+    //   return res.status(400).json({ message: "Invalid form ID" });
+    // }
 
     const formDataUpdates = req.body; // Assuming the updates are in the request body
 
     // Update logic using the provided formDataUpdates
     const result = await db.collection("FormData").updateOne(
-      { _id: id },
+      {  studentId: studentId }, // Assuming you have both formId and studentId in your document as identifiers
       { $set: formDataUpdates }
     );
 
@@ -39,6 +40,9 @@ const updateForm = async (req, res) => {
     console.error("Error updating form data:", error);
     res.status(500).json({ message: "Error updating form data" });
   } 
+  // finally {
+  //   await client.close(); // Use await when closing the connection
+  // }
 };
 
 module.exports = { updateForm };
