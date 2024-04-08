12b56a351a69ab530200783d5843d7cc38697e24
Rawan Abdellatif <rawan.k.abdellatif@gmail.com>
2024-04-08 09:53:16 -0600

    change blog to Events and add GEMinAI tab in navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,10 +43,12 @@ module.exports = {
         // },
         { to: "/intro", label: "Program", position: "left" },
         { to: "/team", label: "Team", position: "left" },
-        {to: "/blog", label:"Blog", position: 'left'},
+        {to: "/blog", label:"Events", position: 'left'},
 
         { to: "/governance", label: "Governance", position: "left" },
+
         { to: "/partners", label: "Partners", position: "left" },
+        { to: "/geminai", label: "GEMinAI", position: "left" },
 
         { href: "/apply", label: "Apply", position: "left" },
         // {
