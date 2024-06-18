7bf60415d2df9424a04c93701ed2d68c174bb441
Rawan Abdellatif <rawan.k.abdellatif@gmail.com>
2024-06-18 12:21:04 -0600

    remove student from team/students.csv

--- src/plugins/custom-webpack-config.js
+++ src/plugins/custom-webpack-config.js
@@ -4,8 +4,8 @@ module.exports = (_context, _options) => ({
       module: {
         rules: [
           {
-            test: /\.js$|jsx/,
-                        loader: "csv-loader",
+            test: /\.csv$/,
+            loader: "csv-loader",
             options: {
               dynamicTyping: true,
               header: true,
