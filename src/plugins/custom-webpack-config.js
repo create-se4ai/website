f0ebf0d3801b2324e26e289598ef39ce2cfefa9c
Rawan Abdellatif <rawan.k.abdellatif@gmail.com>
2024-06-24 10:08:43 -0600

    update custom-webpack

--- src/plugins/custom-webpack-config.js
+++ src/plugins/custom-webpack-config.js
@@ -3,6 +3,7 @@ module.exports = (_context, _options) => ({
     return {
       module: {
         rules: [
+          // Rule for CSV files
           {
             test: /\.csv$/,
             loader: "csv-loader",
@@ -11,6 +12,18 @@ module.exports = (_context, _options) => ({
               header: true,
               skipEmptyLines: true,
             },
+          },
+          // Rule for binary files (images, fonts, etc.)
+          {
+            test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|otf)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]',
+                },
+              },
+            ],
           },
         ],
       },
